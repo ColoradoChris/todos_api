235877c031688dfd4305dc59fdc06e4141d94101
Chris Kansas <ckansas@smcm.edu>
2018-10-03 23:21:00 +0000

    Added in the completion functionality/toggling.

--- public/app.js
+++ public/app.js
@@ -11,20 +11,26 @@ $(document).ready(function(){
        }
     });
     
-    $('.list').on('click', 'span', function(){
+    $('.list').on('click', 'li', function(){
+        updateTodo($(this));
+    });
+    
+    $('.list').on('click', 'span', function(event){
+        event.stopPropagation();
         removeTodo($(this).parent());
     });
 });
 
 function addTodos(todos){
     todos.forEach(function(todo){
-       addTodo(todo);
+        addTodo(todo);
     });
 }
 
 function addTodo(todo){
     var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
     newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
     if(todo.completed){
        newTodo.addClass("done");
     }
@@ -52,6 +58,24 @@ function removeTodo(todo){
     })
     .then(function(data){
         todo.remove();
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+}
+
+function updateTodo(todo){
+    var updateUrl = 'api/todos/' + todo.data('id');
+    var isDone = !todo.data('completed');
+    var updateData = {completed: isDone};
+    $.ajax({
+        method: 'PUT',
+        url: updateUrl,
+        data: updateData
+    })
+    .then(function(updatedTodo){
+        todo.toggleClass('done');
+        todo.data('completed', isDone);
     })
     .catch(function(err){
         console.log(err);
